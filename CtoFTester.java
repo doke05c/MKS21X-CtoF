2e66e4081c54df5c9fc54215263f465fe0251b7f
doke05c <doke30@stuy.edu>
2021-09-23 19:40:06 -0400

    Added F->C!

--- CtoFTester.java
+++ CtoFTester.java
@@ -3,9 +3,17 @@ class CtoFtester{
 		double x = (a * 1.8 + 32);
 		return x;
   }
+ 
+ 	static double fahrenheitToCelsius(double b) {
+		double y = ((b - 32) / 1.8);
+		return y;
+  }
+ 
 	public static void main(String[] args) {
 		double x = celsiusToFahrenheit(0);
+		double y = fahrenheitToCelsius(32);
 		System.out.println(x);
+		System.out.println(y);
 	}
 	
 }
