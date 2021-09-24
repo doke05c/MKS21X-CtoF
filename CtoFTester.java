3c348c4b218fe60f0ec028cf61b25df7b4664fb5
doke05c <doke30@stuy.edu>
2021-09-23 20:05:45 -0400

    Fixed comment placement

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,15 @@
+// Some thoughts.
+/* Q: What should the functions have as parameter(s)? 
+ A: I decided to convert algebrically between one and another. C to F is C*1.8 + 32, and F to C is (F-32)/1.8, so all
+ that was left to do was just turn that into nom nom java monstrosity. (Quite the monstrosity it was, still not used to static and public static void and class and all the rest.
+ Babied by codingbat i suppose. otherwise i guess it was simple.) 
+ Q: What type should they be? 
+ A: Since temperatures come in all messed up forms, I went with double. Unless I want some futuristic near absolute zero degree to say 
+ 0.0000000000000000000000000000000000000000012312893781293712, doubles work just fine for me thanks.
+ Q: What should the functions return? 
+ A: Probably the equivalent value in the other temperature? cToF(0) should give 32, fToC(98.6) should give 37, etc.....
+	end of humor */ 
+
 class CtoFtester{
 	static double celsiusToFahrenheit(double a) {
 		double x = (a * 1.8 + 32);
@@ -25,14 +37,4 @@ class CtoFtester{
 }
 
 
-// Some thoughts.
-/* Q: What should the functions have as parameter(s)? 
- A: I decided to convert algebrically between one and another. C to F is C*1.8 + 32, and F to C is (F-32)/1.8, so all
- that was left to do was just turn that into nom nom java monstrosity. (Quite the monstrosity it was, still not used to static and public static void and class and all the rest.
- Babied by codingbat i suppose. otherwise i guess it was simple. 
- Q: What type should they be? 
- A: Since temperatures come in all messed up forms, I went with double. Unless I want some futuristic near absolute zero degree to say 
- 0.0000000000000000000000000000000000000000012312893781293712, doubles work just fine for me thanks.
- Q: What should the functions return? 
- A: Probably the equivalent value in the other temperature? cToF(0) should give 32, fToC(98.6) should give 37, etc.....
-	end of humor */  
+ 
